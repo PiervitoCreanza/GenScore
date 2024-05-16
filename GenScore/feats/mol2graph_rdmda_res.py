8f8261f96e10dd19a258e512c382ff5b3ba9abf6
Pier107 <p.creanza@protonmail.com>
2024-05-16 11:00:56 +0200

    Script improvements

--- GenScore/feats/mol2graph_rdmda_res.py
+++ GenScore/feats/mol2graph_rdmda_res.py
@@ -18,9 +18,6 @@ from tqdm import tqdm
 from functools import partial
 import math
 
-from rdkit import RDLogger
-RDLogger.DisableLog('rdApp.*')
-
 METAL = ["LI", "NA", "K", "RB", "CS", "MG", "TL", "CU", "AG", "BE", "NI", "PT", "ZN", "CO", "PD", "AG", "CR", "FE", "V",
          "MN", "HG", 'GA',
          "CD", "YB", "CA", "SN", "PB", "EU", "SR", "SM", "BA", "RA", "AL", "IN", "TL", "Y", "LA", "CE", "PR", "ND",
@@ -29,7 +26,8 @@ METAL = ["LI", "NA", "K", "RB", "CS", "MG", "TL", "CU", "AG", "BE", "NI", "PT", 
 RES_MAX_NATOMS = 24
 
 n = 0
-nFailedGraphs = 0
+
+
 def prot_to_graph(prot, cutoff):
     """obtain the residue graphs"""
     u = mda.Universe(prot)
@@ -128,14 +126,17 @@ def obtain_dihediral_angles(res):
 
 
 def calc_res_features(res):
-    return np.array(one_of_k_encoding_unk(obtain_resname(res),
-                                          ['GLY', 'ALA', 'VAL', 'LEU', 'ILE', 'PRO', 'PHE', 'TYR',
-                                           'TRP', 'SER', 'THR', 'CYS', 'MET', 'ASN', 'GLN', 'ASP',
-                                           'GLU', 'LYS', 'ARG', 'HIS', 'MSE', 'CSO', 'PTR', 'TPO',
-                                           'KCX', 'CSD', 'SEP', 'MLY', 'PCA', 'LLP', 'M', 'X']) +  # 32  residue type
-					obtain_self_dist(res) +  # 5
-					obtain_dihediral_angles(res)  # 4
-					)
+    result = np.array(one_of_k_encoding_unk(obtain_resname(res),
+                                            ['GLY', 'ALA', 'VAL', 'LEU', 'ILE', 'PRO', 'PHE', 'TYR',
+                                             'TRP', 'SER', 'THR', 'CYS', 'MET', 'ASN', 'GLN', 'ASP',
+                                             'GLU', 'LYS', 'ARG', 'HIS', 'MSE', 'CSO', 'PTR', 'TPO',
+                                             'KCX', 'CSD', 'SEP', 'MLY', 'PCA', 'LLP', 'M', 'X']) +  # 32  residue type
+                      obtain_self_dist(res) +  # 5
+                      obtain_dihediral_angles(res)  # 4
+                      )
+    if result.size != 41:
+        raise ValueError(f"Unexpected number of features: Got {result.size}")
+    return result
 
 
 def obtain_resname(res):
@@ -157,7 +158,7 @@ def obtain_resname(res):
 ##'FE', 'SR', 'GA', 'IN', 'ZN', 'CU', 'MN', 'SR', 'K' ,'NI', 'NA', 'CD' 'MG','CO','HG', 'CS', 'CA',
 
 def obatin_edge(u, cutoff=10.0):
-    cutoff = 10.0 if cutoff is None else cutoff # Default value not set if None is paased
+    cutoff = 10.0 if cutoff is None else cutoff  # Default value not set if None is paased
     edgeids = []
     dismin = []
     dismax = []
@@ -196,34 +197,42 @@ def calc_dist(res1, res2):
 # return dist_array.max()*0.1, dist_array.min()*0.1
 
 
-def calc_atom_features(atom, explicit_H=False):
+def calc_atom_features(atom, explicit_H):
+    if explicit_H is None: explicit_H = False
     """
     atom: rdkit.Chem.rdchem.Atom
     explicit_H: whether to use explicit H
     use_chirality: whether to use chirality
     """
-    results = one_of_k_encoding_unk(
+    results = (one_of_k_encoding_unk(
         atom.GetSymbol(),
         [
             'C', 'N', 'O', 'S', 'F', 'P', 'Cl',
             'Br', 'I', 'B', 'Si', 'Fe', 'Zn',
             'Cu', 'Mn', 'Mo', 'other'
-        ]) + one_of_k_encoding(atom.GetDegree(),
-                               [0, 1, 2, 3, 4, 5, 6]) + \
-              [atom.GetFormalCharge(), atom.GetNumRadicalElectrons()] + \
-              one_of_k_encoding_unk(atom.GetHybridization(), [
-                  Chem.rdchem.HybridizationType.SP, Chem.rdchem.HybridizationType.SP2,
-                  Chem.rdchem.HybridizationType.SP3, Chem.rdchem.HybridizationType.SP3D,
-                  Chem.rdchem.HybridizationType.SP3D2, 'other']) + [atom.GetIsAromatic()]
+        ]
+    )  # 17
+               +
+               one_of_k_encoding(atom.GetDegree(), [0, 1, 2, 3, 4, 5, 6])  # +7 = 24
+               +
+               [atom.GetFormalCharge(), atom.GetNumRadicalElectrons()]  # +2 = 26
+               +
+               one_of_k_encoding_unk(atom.GetHybridization(), [
+                   Chem.rdchem.HybridizationType.SP, Chem.rdchem.HybridizationType.SP2,
+                   Chem.rdchem.HybridizationType.SP3, Chem.rdchem.HybridizationType.SP3D,
+                   Chem.rdchem.HybridizationType.SP3D2, 'other'])  # + 6 = 32
+               +
+               [atom.GetIsAromatic()])  # + 1 = 33
     # [atom.GetIsAromatic()] # set all aromaticity feature blank.
     # In case of explicit hydrogen(QM8, QM9), avoid calling `GetTotalNumHs`
     if not explicit_H:
         results = results + one_of_k_encoding_unk(atom.GetTotalNumHs(),
-                                                  [0, 1, 2, 3, 4])
+                                                  [0, 1, 2, 3, 4])  # + 5 = 38
     return np.array(results)
 
 
-def calc_bond_features(bond, use_chirality=True):
+def calc_bond_features(bond, use_chirality):
+    if use_chirality is None: use_chirality = True
     """
     bond: rdkit.Chem.rdchem.Bond
     use_chirality: whether to use chirality
@@ -242,23 +251,29 @@ def calc_bond_features(bond, use_chirality=True):
     return np.array(bond_feats).astype(int)
 
 
-def load_mol(molpath, explicit_H=False, use_chirality=True):
+def load_mol(molpath, explicit_H, use_chirality):
+    if explicit_H is None: explicit_H = False
+    if use_chirality is None: use_chirality = True
     # load mol
     if re.search(r'.pdb$', molpath):
-        mol = Chem.MolFromPDBFile(molpath, removeHs=not explicit_H)
+        mol = Chem.MolFromPDBFile(molpath, removeHs=explicit_H, sanitize=False)
     elif re.search(r'.mol2$', molpath):
-        mol = Chem.MolFromMol2File(molpath, removeHs=not explicit_H)
+        mol = Chem.MolFromMol2File(molpath, removeHs=not explicit_H, sanitize=False)
     elif re.search(r'.sdf$', molpath):
-        mol = Chem.MolFromMolFile(molpath, removeHs=not explicit_H)
+        mol = Chem.MolFromMolFile(molpath, removeHs=not explicit_H, sanitize=False)
     else:
         raise IOError("only the molecule files with .pdb|.sdf|.mol2 are supported!")
 
+    if mol is None:
+        raise TypeError("Molecule is None in mol2graph_rdmda_res")
     if use_chirality:
         Chem.AssignStereochemistryFrom3D(mol)
     return mol
 
 
-def mol_to_graph(mol, explicit_H=False, use_chirality=True):
+def mol_to_graph(mol, explicit_H, use_chirality):
+    if explicit_H is None: explicit_H = False
+    if use_chirality is None: use_chirality = True
     """
 	mol: rdkit.Chem.rdchem.Mol
 	explicit_H: whether to use explicit H
@@ -309,6 +324,8 @@ def mol_to_graph(mol, explicit_H=False, use_chirality=True):
 
 
 def mol_to_graph2(prot_path, lig_path, cutoff=10.0, explicit_H=False, use_chirality=True):
+    if explicit_H is None: explicit_H = False
+    if use_chirality is None: use_chirality = True
     prot = load_mol(prot_path, explicit_H=explicit_H, use_chirality=use_chirality)
     lig = load_mol(lig_path, explicit_H=explicit_H, use_chirality=use_chirality)
     gp = prot_to_graph(prot, cutoff)
@@ -317,28 +334,39 @@ def mol_to_graph2(prot_path, lig_path, cutoff=10.0, explicit_H=False, use_chiral
 
 
 def label_query(pdbid, df):
-    global n
-    n += 1
-    return n
+    logKd = df.loc[pdbid, "-logKd/Ki"]
+    return logKd
 
+def element_in_file(file_path, element):
+    with open(file_path, 'r') as file:
+        for line in file:
+            if element in line:
+                return True
+    return False
 
-# return df.loc[pdbid, "labels"]
 
 def pdbbind_handle(pdbid, args):
-    prot_path = "%s/%s/%s_pocket.ligen.pdb" % (args.dir, pdbid, pdbid)
+    if args.is_davide_set:
+        prot_path = "%s/%s/%s_pocket.ligen.pdb" % (args.dir, pdbid, pdbid)
+    else:
+        prot_path = "%s/%s/%s_pocket.pdb" % (args.dir, pdbid, pdbid)
     lig_path = "%s/%s/%s_ligand.mol2" % (args.dir, pdbid, pdbid)
+    # If we work on the palermo set we use both the general and refined set, so we have to set the path accordingly.
+    if args.refined_csv:
+        if not element_in_file(args.refined_csv, pdbid):
+            prot_path = "%s/%s/%s_pocket.pdb" % (args.general_set_dir, pdbid, pdbid)
+            lig_path = "%s/%s/%s_ligand.mol2" % (args.general_set_dir, pdbid, pdbid)
+
     try:
         gp, gl = mol_to_graph2(prot_path,
-                                lig_path,
-                                cutoff=args.cutoff,
-                                explicit_H=args.useH,
-                                use_chirality=args.use_chirality)
+                               lig_path,
+                               cutoff=args.cutoff,
+                               explicit_H=args.useH,
+                               use_chirality=args.use_chirality)
     except:
-        #print("%s failed to generare the graph"%pdbid)
-        global nFailedGraphs
-        nFailedGraphs += 1
+        print("%s failed to generare the graph"%pdbid)
         gp, gl = None, None
-        #gm = None
+        # gm = None
     return pdbid, gp, gl, label_query(pdbid, args.ref)
 
 
@@ -346,7 +374,9 @@ def UserInput():
     import argparse
     p = argparse.ArgumentParser()
     p.add_argument('-d', '--dir', default=".",
-                   help='The directory to store the protein-ligand complexes.')
+                   help='The directory of the refined set')
+    p.add_argument('-dg', '--general_set_dir', default=".",
+                   help='The directory of the general set')
     p.add_argument('-c', '--cutoff', type=float,
                    help='the cutoff to determine the pocket')
     p.add_argument('-o', '--outprefix', default="out",
@@ -360,23 +390,46 @@ def UserInput():
     p.add_argument('-p', '--parallel', default=False, action="store_true",
                    help='whether to obtain the graphs in parallel (When the dataset is too large,\
 						 it may be out of memory when conducting the parallel mode).')
+    p.add_argument('-fs' '--filter_set', help="The csv file from wich you want to get the filtered pdbids")
+    p.add_argument('-rc', '--refined_csv', help="The csv file containing data about refined pdbids")
+    p.add_argument('-ds', '--is_davide_set', help="Whether we are using the Davide set or not.", action="store_true")
 
     args = p.parse_args()
     return args
 
+def get_pdbids(args):
+    palermoSet = pd.read_csv(args.fs__filter_set)
+    pdbids = [x for x in os.listdir(args.dir) if os.path.isdir("%s/%s" % (args.dir, x))]
+    res = palermoSet[palermoSet["energy_mean"]< -10]["pdb"].tolist()
+    if (any(elem in pdbids for elem in res) == False):
+        raise Exception("Some pdbids were not found")
+    return res
+
 def main():
     args = UserInput()
-    pdbids = [x for x in os.listdir(args.dir) if os.path.isdir("%s/%s" % (args.dir, x))]
-    # args.ref = pd.read_csv(args.ref, index_col=0, header=0)
-    # results = []
-    # results.append(pdbbind_handle(pdbids[0], args))
+    if args.fs__filter_set:
+        # If we are working on the palermo set we need to filter the proteins
+        pdbids = get_pdbids(args)
+        a = 0
+        prots = []
+        for pdbid in pdbids:
+            if not element_in_file(args.refined_csv, pdbid):
+                a = a+1
+                prots.append(pdbid)
+        print("Not in pdbind", a)
+    else:
+        # If we work on the standard refine set
+        pdbids = [x for x in os.listdir(args.dir) if os.path.isdir("%s/%s" % (args.dir, x))]
+
+    print("Found ", len(pdbids), "proteins")
+    args.ref = pd.read_csv(args.ref, index_col=0, header=0)
     if args.parallel:
         print("Starting in multi process mode")
-        #results = Parallel(n_jobs=-1)(delayed(pdbbind_handle)(pdbid, args) for pdbid in pdbids)
+        results = Parallel(n_jobs=-1)(delayed(pdbbind_handle)(pdbid, args) for pdbid in pdbids)
         # Create a partial function with the constant parameter
-        partial_task = partial(pdbbind_handle, args=args)
-        with Pool(processes=1) as pool:
-            pool.imap(partial_task, pdbids, chunksize=math.ceil(len(pdbids) / 2))
+        # partial_task = partial(pdbbind_handle, args=args)
+        # with Pool(processes=1) as pool:
+        #    pool.imap(partial_task, pdbids, chunksize=math.ceil(len(pdbids) / 2))
     else:
         print("Starting in single process mode")
         results = []
@@ -385,13 +438,14 @@ def main():
                 results.append(pdbbind_handle(pdbid, args))
                 bar()
     results = list(filter(lambda x: x[1] is not None, results))
+    print("N of proteins:  ", len(pdbids))
+    print("\033[31m", "N of failed prots: ", len(pdbids) - len(results), "\033[0m")
+    print("\033[32m", "N of converted prots: ", len(results), "\033[0m")
     ids, graphs_p, graphs_l, labels = list(zip(*results))
     np.save("%s_ids" % args.outprefix, (ids, labels))
     th.save(graphs_p, "%s_prot.pt" % args.outprefix)
     th.save(graphs_l, "%s_lig.pt" % args.outprefix)
-    print("Errored proteins:", nFailedGraphs)
 
 
 if __name__ == '__main__':
     main()
-
